461bde5d2ac0b7b35c3d78d71a6b3ef6a4d18493
Mithil Pawar <mithilpawar7044@gmail.com>
2025-01-13 22:01:18 +0530

    Your commit message

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // For React
+    "./public/index.html",       // For plain HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
